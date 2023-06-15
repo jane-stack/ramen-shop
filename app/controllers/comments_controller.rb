3cba1aa5179465d2d21fe917fd92ce104fc38fff
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-15 14:12:27 -0600

    all controllers are done

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,37 @@
 class CommentsController < ApplicationController
-    belongs_to :user
-    belongs_to :review
+    before_action :find_review
+    before_action :authorize, only: [:create]
+
+    def index
+        @review = Review.find(params[:review_id])
+        @comment = @review.comments
+        render json: @comment
+    end
+
+    def create
+        @comment = @review.comments.create(comment_params)
+        @comment.user = current_user
+        if @comment.save
+            render json: @comment, status: 201
+        else
+            render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @comment = @review.comments.find(params[:id])
+        @comment.destroy
+        render json: @comment
+    end
+
+    private
+    
+    def comment_params
+        params.premit(:body)
+    end
+
+    def find_blog
+        @review = Review.find(params[:review_id])
+    end
+
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,52 @@
 class ReviewsController < ApplicationController
+    before_action :find_review, only: [:update, :destroy]
+    before_action :unprocessable_entity_if_not_found, only: [:update, :destroy]
+    skip_before_action :authorize, only: [:index]
+    before_action only: [:update, :destroy] do
+        authorize_user_resource(@review.user_id)
+    end
+
+    def index
+        if params[:user_id]
+            user = User.find_by_id(params[:user_id])
+            @reviews = user.reviews
+        else
+            @reviews = Review.all
+        end
+        render json: @reviews
+    end
+
+    def create
+        review = current_user.reviews.create(review_params)
+        if review.valid?
+            render json: review
+        else
+            render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        @review.update(review_params)
+        render json: @review
+    end
+
+    def destroy
+        @review.destroy
+        render json: @review
+    end
+
+    private
+
+    def review_params
+        params.premit(:content)
+    end
+
+    def find_review
+        @review = Review.find_by_id(params[:id])
+    end
+
+    def unprocessable_entity_if_not_found
+        render json: { message: "Review not found" }, status: :unprocessable_entity unless @review
+    end
+
 end
