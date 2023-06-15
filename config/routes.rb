1de042f161980d6c0a4e4cedc683aef3560890ba
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-06-15 14:15:38 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :reviews
   resources :menus
-  resources :users
+  resources :users, only, [:index]
+  resources :reviews do
+    resources :comments, only: [:index, :create, :destroy]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # User
+
+  post '/signup', to: 'users#create'
+  get '/me', to: 'users#show'
+
+  # Sessions
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # Defines the root path route ("/")
   # root "articles#index"
